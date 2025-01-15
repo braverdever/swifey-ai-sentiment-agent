e20daf83b68720a66110a5d7d5643e64558e3a85
braverdever <tinydever@gmail.com>
2025-01-16 03:32:10 +0530

    small fix

--- app/main.py
+++ app/main.py
@@ -162,7 +162,7 @@ async def auth_middleware_handler(request: Request, call_next):
         "/api/v1/profile/verify-invite-code",
         "/api/v1/profile/create-invitation",
         "/api/v1/profile/me",
-        "/api/v1/profile/invite-codes/{profile_id}",
+        "/api/v1/profile/invite-codes",
         "/api/v1/profile/report",
         "/api/v1/profile/matched_profiles",
         "/api/v1/profile/invite-code",
